dbac3f0bade3203f60e9a09fdae3510a09ebdc11
aha <1092739233@qq.com>
2020-05-29 20:12:32 +0800

    style: 修改响应结果类名称

--- src/main/java/online/ahayujie/project/security/JwtAccessDeniedHandler.java
+++ src/main/java/online/ahayujie/project/security/JwtAccessDeniedHandler.java
@@ -3,12 +3,11 @@ package online.ahayujie.project.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import online.ahayujie.project.core.Result;
-import online.ahayujie.project.core.ResultCode;
+import online.ahayujie.project.core.ResultCodeEnum;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -29,7 +28,7 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
         response.setHeader("Content-type", "application/json;charset=UTF-8");
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         ObjectMapper objectMapper = new ObjectMapper();
-        response.getWriter().write(objectMapper.writeValueAsString(Result.fail(ResultCode.FORBIDDEN)));
+        response.getWriter().write(objectMapper.writeValueAsString(Result.fail(ResultCodeEnum.FORBIDDEN)));
     }
 
 }
--- src/main/java/online/ahayujie/project/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/online/ahayujie/project/security/JwtAuthenticationEntryPoint.java
@@ -3,12 +3,11 @@ package online.ahayujie.project.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import online.ahayujie.project.core.Result;
-import online.ahayujie.project.core.ResultCode;
+import online.ahayujie.project.core.ResultCodeEnum;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -29,7 +28,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
         response.setHeader("Content-type", "application/json;charset=UTF-8");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         ObjectMapper objectMapper = new ObjectMapper();
-        response.getWriter().write(objectMapper.writeValueAsString(Result.fail(ResultCode.UNAUTHORIZED)));
+        response.getWriter().write(objectMapper.writeValueAsString(Result.fail(ResultCodeEnum.UNAUTHORIZED)));
     }
 
 }
